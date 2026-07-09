2c64896d204e513cf3cc3d0e2fe8cca445b17afa
jolicajean-bit <jolica.jean@escola.pr.gov.br>
2026-07-09 09:25:37 -0300

    s

--- main.js
+++ main.js
@@ -19,5 +19,13 @@ function aumentaTamanho(){
     if (tamanhoSenha < 20){
        tamanhoSenha = tamanhoSenha+1;
     }
-    numeroSenha.textContent = tamanhoSenha;
+    numeroSenha.textContent = tamanhoSenha;geraSenha();
+}
+function aumentaTamanho() {
+if (tamanhoSenha < 20) {
+// tamanhoSenha = tamanhoSenha+1;
+tamanhoSenha++;
+}
+numeroSenha.textContent = tamanhoSenha;
+geraSenha();
 }
